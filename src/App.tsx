c6e5dc5cbb409a75a565d2f81185523a4d5f39a6
Gabriel Pavão <gabpavao@outlook.com>
2023-05-26 07:51:24 -0300

    feat: create contact list HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ClientProvider } from './contexts/ClientContext'
+import { CgProfile } from 'react-icons/cg'
 
 export function App() {
 	return (
@@ -7,11 +8,25 @@ export function App() {
 			<header>
 				<h1>Desafio Tech</h1>
 				<div>
-					<p>Name: <span>Gabriel Galvão Pavão</span></p>
+					<p>Nome: <span>Gabriel Galvão Pavão</span></p>
 					<p>Email: <span>gabriel@mail.com</span></p>
 					<p>Telefone: <span>12345678901</span></p>
 				</div>
 			</header>
+
+			<h2>Seus Contatos</h2>
+			<main>
+				<ul>
+					<li>
+						<CgProfile/>
+						<div>
+							<p>Nome: <span>André Gomes</span></p>
+							<p>Email: <span>andre@mail.com</span></p>
+							<p>Telefone: <span>12345678901</span></p>
+						</div>
+					</li>
+				</ul>
+			</main>
 		</ClientProvider>
 	)
 }
