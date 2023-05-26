07f24983aaaa89a79afd252e30d174c251a910a8
Gabriel Pavão <gabpavao@outlook.com>
2023-05-25 21:05:41 -0300

    feat: create home page header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import { ClientProvider } from './contexts/ClientContext'
 export function App() {
 	return (
 		<ClientProvider>
+			<header>
+				<h1>Desafio Tech</h1>
+				<div>
+					<p>Name: <span>Gabriel Galvão Pavão</span></p>
+					<p>Email: <span>gabriel@mail.com</span></p>
+					<p>Telefone: <span>12345678901</span></p>
+				</div>
+			</header>
 		</ClientProvider>
 	)
 }
