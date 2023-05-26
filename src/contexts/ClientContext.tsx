09a1bcfcd8121b9f4e3f2c772b300eee0226042f
Gabriel Pavão <gabpavao@outlook.com>
2023-05-26 19:47:17 -0300

    feat: create function to retrive client info

--- src/contexts/ClientContext.tsx
+++ src/contexts/ClientContext.tsx
@@ -1,9 +1,26 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect } from 'react'
 import { iClientProviderValue, iProviderProps } from './interfaces'
+import { api } from '../services/api'
 
 export const ClientContext = createContext({} as iClientProviderValue)
 
 export function ClientProvider({ children }: iProviderProps) {
+	async function retrieveClientInfo() {
+		try {
+			const response = await api.get(`clients/${1}`)
+			return response.data
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		async function handleRetrieveClientInfo() {
+			await retrieveClientInfo()
+		}
+		handleRetrieveClientInfo()
+	}, [])
+
 	return (
 		<ClientContext.Provider value={{}}>
 			{children}
