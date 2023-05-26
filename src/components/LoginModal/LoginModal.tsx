c98f1e37364648d94e1ce5364bcdf5ad97405b5d
Gabriel Pavão <gabpavao@outlook.com>
2023-05-26 20:16:51 -0300

    feat: useForm to submit login

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BaseModal } from '../BaseModal/BaseModal'
 import { StyledForm } from '../../styles/StyledForm'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { ClientContext } from '../../contexts/ClientContext'
+
+type FormData = {
+    email: string;
+    password: string;
+};
 
 export function LoginModal() {
+	const { register, handleSubmit, formState: { errors }} = useForm<FormData>({
+		mode: 'onBlur'
+	})
+
+	const { login } = useContext(ClientContext)
+
+	const submitLogin : SubmitHandler<FormData> = (data) => {
+		login(data)
+	}
+
 	return (
 		<BaseModal>
-			<StyledForm>
+			<StyledForm onSubmit={handleSubmit(submitLogin)} noValidate>
 				<fieldset>
 					<label htmlFor='loginEmail'>Email</label>
-					<input id='loginEmail' type='email' placeholder='Digite seu email...'/>
+					<input id='loginEmail' type='email' placeholder='Digite seu email...' {...register('email')}/>
 				</fieldset>
 
 				<fieldset>
 					<label htmlFor='loginPassword'>Senha</label>
-					<input id='loginPassword' type='password' placeholder='Digite sua senha...'/>
+					<input id='loginPassword' type='password' placeholder='Digite sua senha...' {...register('password')}/>
 				</fieldset>
 
 				<button type='submit'>Submeter</button>
