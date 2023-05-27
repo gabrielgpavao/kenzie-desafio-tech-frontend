03f27cd6e00d489e630d8dfb73457152572ce506
Gabriel Pavão <gabpavao@outlook.com>
2023-05-27 14:25:32 -0300

    feat: add contact hook-form

--- src/components/AddContact/AddContact.tsx
+++ src/components/AddContact/AddContact.tsx
@@ -1,23 +1,35 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useClient } from '../../hooks/useClient'
 import { StyledForm } from '../../styles/StyledForm'
 
 export function AddContact() {
+	const { createContact } = useClient()
+
+	const { register, handleSubmit } = useForm<any>({
+		mode: 'onSubmit'
+	})
+
+	const submitContact: SubmitHandler<any> = async (data) => {
+		await createContact(data)
+	}
+
 	return (
 		<section>
 			<h3>Adicionar novo contato</h3>
-			<StyledForm>
+			<StyledForm onSubmit={handleSubmit(submitContact)}>
 				<fieldset>
 					<label htmlFor='fullName'>Nome Completo</label>
-					<input id='fullName' type='text' placeholder='Digite o nome completo...'/>
+					<input id='fullName' type='text' placeholder='Digite o nome completo...' {...register('fullName')}/>
 				</fieldset>
 
 				<fieldset>
 					<label htmlFor='email'>Email</label>
-					<input id='email' type='email' placeholder='Digite o email...'/>
+					<input id='email' type='email' placeholder='Digite o email...' {...register('email')}/>
 				</fieldset>
 
 				<fieldset>
-					<label htmlFor='phoneNumber'>Telefone</label>
-					<input id='phoneNumber' type='text' placeholder='Digite o telefone...'/>
+					<label htmlFor='c'>Telefone</label>
+					<input id='phoneNumber' type='text' placeholder='Digite o telefone...' {...register('phoneNumber')}/>
 				</fieldset>
 
 				<button type='submit'>Adicionar</button>
