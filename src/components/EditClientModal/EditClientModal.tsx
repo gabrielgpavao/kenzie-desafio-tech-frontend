5daeb7b6dd4cf0a1ea1c927c36495f0428e66544
Gabriel <gabpavao@outlook.com>
2023-05-28 15:31:51 -0300

    feat: create modal to edit or delete client

---
+++ src/components/EditClientModal/EditClientModal.tsx
@@ -0,0 +1,45 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { StyledForm } from '../../styles/StyledForm'
+import { BaseModal } from '../BaseModal/BaseModal'
+import { useContact } from '../../hooks/useContact'
+
+export function EditClientModal() {
+	const { register, handleSubmit } = useForm<any>({
+		mode: 'onSubmit'
+	})
+
+	const { setIsEditContactModalOpen } = useContact()
+
+	const submitUpdate : SubmitHandler<any> = async (data) => {
+		console.log(data)
+	}
+
+	return (
+		<BaseModal>
+			<StyledForm onSubmit={handleSubmit(submitUpdate)}>
+				<div className='modalHeader'>
+					<h2>Editar Minhas Informações</h2>
+					<span onClick={() => { setIsEditContactModalOpen(false) }}>X</span>
+				</div>
+				<fieldset>
+					<label htmlFor='editContactFullName'>Nome Completo</label>
+					<input id='editContactFullName' type='text' placeholder='Digite o nome completo...' defaultValue={'contactInfo.fullName'} {...register('fullName')}/>
+				</fieldset>
+
+				<fieldset>
+					<label htmlFor='editContactEmail'>Email</label>
+					<input id='editContactEmail' type='email' placeholder='Digite o email...' defaultValue={'contactInfo.email'} {...register('email')}/>
+				</fieldset>
+
+				<fieldset>
+					<label htmlFor='editContactPhoneNumber'>Telefone</label>
+					<input id='editContactPhoneNumber' type='text' placeholder='Digite o telefone...' defaultValue={'contactInfo.phoneNumber'} {...register('phoneNumber')}/>
+				</fieldset>
+
+				<button type='submit'>Editar</button>
+
+				<span>Deletar Conta</span>
+			</StyledForm>
+		</BaseModal>
+	)
+}
